03db9d01a86f089ecd6c54f14c0ed3acfcb02cb3
Olivia Lewis <ollybaby12@gmail.com>
2021-03-10 14:12:11 -0500

    add to validate mw func, add pokedex.json

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, new world!')
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    console.log('validate bearer token middleware')
+
+    if (!authToken || authToken.split(' ')[1] !==apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+    //move to the next middleware
+    next()
 })
+
+function handleGetTypes(req, res) {
+    res.json(validTypes)
+
+}
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000
 
