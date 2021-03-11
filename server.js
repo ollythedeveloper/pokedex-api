0ee550a765c37ee3edb36a592844df082d30d054
Olivia Lewis <ollybaby12@gmail.com>
2021-03-11 12:16:29 -0500

    update morgan setting

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const  POKEDEX = require('./pokedex.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV == 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
