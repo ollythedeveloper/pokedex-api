33d0ef73d2fcffcc031ee855d6c812e5c9d03211
Olivia Lewis <ollybaby12@gmail.com>
2021-03-11 12:23:40 -0500

    removed console logs

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
 
-    console.log('validate bearer token middleware')
-
     if (!authToken || authToken.split(' ')[1] !==apiToken) {
         return res.status(401).json({ error: 'Unauthorized request'})
     }
@@ -60,7 +58,5 @@ app.get('/pokemon', handleGetPokemon)
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+app.listen(PORT)
 
